e159f7b42c43ded1c250e258c69f3804f095a40e
hasibulhasananik40420 <hasibulhasananik40420@gmail.com>
2024-02-10 20:06:48 +0600

    updated

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -86,7 +86,7 @@ const Home = () => {
           WORK samples
           </h4>
 
-          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-3 gap-6 mt-[66px]">
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-3 gap-6 mt-[32px]">
             {projects.map((project, index) => (
               <div
                 key={project._id}
