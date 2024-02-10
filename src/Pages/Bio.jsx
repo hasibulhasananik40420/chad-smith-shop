9072af84ebbe5fbc0b6bd2a685c0387c6f905049
hasibulhasananik40420 <hasibulhasananik40420@gmail.com>
2024-02-10 21:14:10 +0600

    updated

--- src/Pages/Bio.jsx
+++ src/Pages/Bio.jsx
@@ -24,13 +24,13 @@ const Bio = () => {
                         <div className="flex gap-3">
                   
                          <div className="h-[2px] w-[10px] mt-2 bg-black"></div>
-                        <p className="text-[#080808] text-[14px] font-Poppins font-normal leading-6 ">Contemporary Artist  born in Washington, DC, 1988</p>
+                        <p className="text-[#080808] text-[14px] font-Poppins font-light leading-6 ">Contemporary Artist  born in Washington, DC, 1988</p>
                         </div>
 
 
                         <div className="flex gap-3">
                         <div className="h-[2px] w-[13px] mt-2 bg-black"></div>
-                        <p className="text-[#080808] text-[14px] font-Poppins font-normal leading-6 ">Bachelor's Degree from Hampton University, 2010</p>
+                        <p className="text-[#080808] text-[14px] font-Poppins font-light leading-6 ">Bachelor's Degree from Hampton University, 2010</p>
                         </div>
 
                         
@@ -38,15 +38,15 @@ const Bio = () => {
 
                         <div className="flex  gap-3">
                         <div className="h-[2px] w-[12px] mt-2 bg-black"></div>
-                        <p className="text-[#080808] text-[14px] font-Poppins font-normal leading-6 ">Current Medium: Collaboration, 3D Printing & Photography</p>
+                        <p className="text-[#080808] text-[14px] font-Poppins font-light leading-6 ">Current Medium: Collaboration, 3D Printing & Photography</p>
                         </div>
 
 
 
-                     <div className="">
+                     <div className="mt-5">
                          <h1 className="text-[24px] text-[#080808] font-Poppins font-semibold leading-[30px]">Artist Statement</h1>
 
-                        <p className="text-[#080808] mt-3 text-[14px] font-Poppins font-normal leading-6 ">I've always been interested in working across mediums. In my mid-20s, I began collaborating with other artists in the medium of Video Art. Using Collaboration as a tool allowed me to enter other artist's styles, and extend my own style, to create something unachievable alone. I've currently landed in 3D Printing and Photography, collaborating with illustrators, animators and 3D printing professionals, to build a collection of new work. "Self-portrait", "Please help me", and "A talk on the mind" are a few of my favorites from the collection so far.</p>
+                        <p className="text-[#080808] mt-3 text-[14px] font-Poppins font-light leading-6 ">I've always been interested in working across mediums. In my mid-20s, I began collaborating with other artists in the medium of Video Art. Using Collaboration as a tool allowed me to enter other artist's styles, and extend my own style, to create something unachievable alone. I've currently landed in 3D Printing and Photography, collaborating with illustrators, animators and 3D printing professionals, to build a collection of new work. "Self-portrait", "Please help me", and "A talk on the mind" are a few of my favorites from the collection so far.</p>
                         </div>
 
 
